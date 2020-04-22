60aa04f724b836baf4dd5b3c2f66ca2cac476ff4
Rahul Agrawal <41531498+rahul799@users.noreply.github.com>
2020-04-23 00:02:11 +0530

    Delete style.sh

--- scripts/style.sh
+++
@@ -1,7 +0,0 @@
- #! /bin/bash
-
-echo '${{ steps.file_changes.outputs.files_modified}}' > names.txt
-cat names.txt | tr -d '[]' > new.txt
-text=$(cat new.txt)
-IFS=',' read -ra ids <<< "$text"
-for i in "${ids[@]}"; do if [[ "$i" == *.R\" || "$i" == *.Rmd\" ]]; then echo "$i" >> new2.txt; fi; done
